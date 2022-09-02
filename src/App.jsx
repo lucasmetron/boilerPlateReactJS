352faa0553fe527b109c6fc1d82c4984ab3c8e2f
lucasmetron <lucasmetron@gmail.com>
2022-09-02 11:10:35 -0300

    husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import logo from './logo.svg'
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +24,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
